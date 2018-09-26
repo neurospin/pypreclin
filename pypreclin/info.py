fb2354f3cd4b5f5641bca1b8b79cf1624b6c9305
Antoine Grigis <antoine.grigis@cea.fr>
2018-09-26 14:09:28 +0200

    pypreclin/info: add new dependency.

--- pypreclin/info.py
+++ pypreclin/info.py
@@ -84,7 +84,8 @@ REQUIRES = [
     "pyconnectomist>=2.0.0",
     "nipype>=1.0.1",
     "matplotlib>=1.5.1",
-    "nibabel>=1.1.0"]
+    "nibabel>=1.1.0",
+    "filelock"]
 EXTRA_REQUIRES = {
     "wip": {
         "pypreprocess>=0.1.0"
